9f761938c66e0848b5e16a203b0598c3fe6bb329
nknighta <n.knight.pc0627@gmail.com>
2025-06-25 12:07:03 +0900

    update: 👆 fix redirect

--- src/pages/l/[linkid].tsx
+++ src/pages/l/[linkid].tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import Layout from "../../layout/main";
 import { useRouter } from "next/router";
 
@@ -7,23 +6,14 @@ export default function Link() {
   const { linkid } = router.query;
 
   if (!linkid) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>; // Handle loading state
   }
 
-  const redirecthandler = () => {
-    if (linkid === "vx") {
-      useEffect(() => {
-        window.location.href = "https://github.com/nknighta/vx/pkgs/npm/vx";
-      }, []);
-    }
-  };
-
-  redirecthandler();
+    linkid === "vx" && router.push("https://github.com/nknighta/vx/pkgs/npm/vx");
   return (
     <Layout>
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>Redirecting...</h1>
-        <p>You are being redirected to the VX package page.</p>
+      <div style={{ textAlign: "center", marginTop: "50px" , height: "100vh"}}>
+        <h1>Redirect to {linkid}</h1>
       </div>
     </Layout>
   );
