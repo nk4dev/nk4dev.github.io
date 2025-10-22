fee4399f2537d311774119da4c8d9cba5aa59fb9
nknighta <nknighta@varius.technology>
2025-10-22 14:37:17 +0900

    update: 👆 image
    add scrennshot

--- src/pages/dev/make-a-os.tsx
+++ src/pages/dev/make-a-os.tsx
@@ -2,6 +2,7 @@ import Layout from "../../layout/main";
 import Link from "next/link";
 import { css } from "../../../styled-system/css";
 import HMeta from "../../components/headermeta";
+import Image from "next/image";
 
 function MakeAOS() {
   return (
@@ -9,6 +10,7 @@ function MakeAOS() {
       <HMeta pageTitle="Make My OS" pageDescription="Development log for making my own operating system using Rust." />
       <div className={css({ display: "flex", flexDirection: "column", gap: "12px", margin: "auto", maxWidth: "800px", padding: "20px" })}>
         <h1 className={css({ fontSize: "32px", fontWeight: "bold" })}>Make My OS</h1>
+        <Image src="/images/os-screenshot.png" alt="Make My OS" width={800} height={400} />
         <h2 className={css({ fontSize: "25px" })}>Language</h2>
         <p className={css({ fontSize: "16px" })}>
           Rust (Nightly)
@@ -17,8 +19,8 @@ function MakeAOS() {
         <Link target="_blank" href="https://zenn.dev/nknight_amamiya/scraps/8fc44914bd6ed1">
           Open on zenn.dev
         </Link>
-        <Link target="_blank" href="#">
-          Repository (Coming soon)
+        <Link target="_blank" href="https://github.com/nk4dev/my-os">
+          Repository (nk4dev/my-os)
         </Link>
         <h2 className={css({ fontSize: "25px" })}>References</h2>
         <Link target="_blank" href="https://os.phil-opp.com/">
