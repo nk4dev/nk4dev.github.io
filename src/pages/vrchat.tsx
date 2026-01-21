10418e8d5d2cb2c5afce882b768f949bf7f0815b
nk4dev <nknighta@varius.technology>
2026-01-21 16:51:35 +0900

    fix: 🐛 add ogp image

--- src/pages/vrchat.tsx
+++ src/pages/vrchat.tsx
@@ -8,6 +8,9 @@ export default function VRCRedirect() {
       <HMeta
         pageTitle="Nknight AMAMIYA | VRChat"
         pageDescription="Nknight AMAMIYA's VRChat Profile"
+        pageImg="https://images.microcms-assets.io/assets/a2939c8d25434ae5a1f853f2dc239a0f/d17eaece880442c78ebf7fbad2773041/VRChat_2026-01-21_14-24-23.727_2560x1440.png"
+        pageImgHeight={1440}
+        pageImgWidth={2560}
         pagePath="/vrchat"
       />
       <LinkPage url="https://vrchat.com/home/user/usr_3c0e5ebc-16db-4f61-bdfb-88ff8385a7d4" text="VRChat" />
