735d3bd40fb17a621fc4f0dd49c4bc38d31c40d3
nk4dev <nknighta@varius.technology>
2026-01-21 15:17:09 +0900

    fix: 🐛 metadata

--- src/pages/vrchat.tsx
+++ src/pages/vrchat.tsx
@@ -6,8 +6,8 @@ export default function VRCRedirect() {
   return (
     <Layout>
       <HMeta
-        pageTitle="VRChat Redirect"
-        pageDescription="Redirect to VRChat profile"
+        pageTitle="Nknight AMAMIYA | VRChat"
+        pageDescription="Nknight AMAMIYA's VRChat Profile"
         pagePath="/vrchat"
       />
       <LinkPage url="https://vrchat.com/home/user/usr_3c0e5ebc-16db-4f61-bdfb-88ff8385a7d4" text="VRChat" />
