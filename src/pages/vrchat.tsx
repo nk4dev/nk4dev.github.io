1ceffe52c6fb965df67b4b8308253554b3a2508b
nk4dev <nknighta@varius.technology>
2026-01-21 15:12:08 +0900

    feat: 🎸 add metadata on /vrchat

--- src/pages/vrchat.tsx
+++ src/pages/vrchat.tsx
@@ -1,9 +1,15 @@
 import Layout from "../layout/main";
 import LinkPage from "../components/redirect";
+import HMeta from "../components/headermeta";
 
 export default function VRCRedirect() {
   return (
     <Layout>
+      <HMeta
+        pageTitle="VRChat Redirect"
+        pageDescription="Redirect to VRChat profile"
+        pagePath="/vrchat"
+      />
       <LinkPage url="https://vrchat.com/home/user/usr_3c0e5ebc-16db-4f61-bdfb-88ff8385a7d4" text="VRChat" />
     </Layout>
   );
