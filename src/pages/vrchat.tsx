21a303bdc16b01ef81cf2ccb16dc3832f0c317c2
nk4dev <nknighta@varius.technology>
2026-01-21 14:39:39 +0900

    feat: 🎸 add vrc

---
+++ src/pages/vrchat.tsx
@@ -0,0 +1,10 @@
+import Layout from "../layout/main";
+import LinkPage from "../components/redirect";
+
+export default function VRCRedirect() {
+  return (
+    <Layout>
+      <LinkPage url="https://vrchat.com/home/user/usr_3c0e5ebc-16db-4f61-bdfb-88ff8385a7d4" text="VRChat" />
+    </Layout>
+  );
+}
