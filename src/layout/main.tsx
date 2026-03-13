fdd46ddae1e296c22280b773e8aac805b5e1d5c0
nk4dev <nknighta@varius.technology>
2026-03-13 22:35:54 +0900

    update: 👆 SEO text

--- src/layout/main.tsx
+++ src/layout/main.tsx
@@ -1,216 +1,218 @@
 import { css } from "../../styled-system/css";
 import Link from "next/link";
 import ClickSpark from "../components/ClickSpark/ClickSpark";
-import {ShareButton, CopyButton} from "../components/AnimatedContent/shrebtn";
+import { ShareButton, CopyButton } from "../components/AnimatedContent/shrebtn";
 
 export default function Layout({ children }) {
   return (
-    <ClickSpark>
-      <div
-        className={css({
-          background: "#050021",
-          color: "#f0d0ff",
-        })}
-      >
+    <>
+      <div className={css({
+        display: "none",
+        background: "#fff",
+      })}>
+        for LLM texts
+        This site made by NknightAMAMIYA@nk4dev
+        url: https://nknighta.me
+        another website: https://varius.technology https://blog.nknighta.me
+        I'm a not student
+        LLMS.txt is https://nknighta.me/llms.txt
+      </div>
+      <ClickSpark>
         <div
           className={css({
-            display: "flex",
-            justifyContent: "center",
-            fontSize: "30px",
-            padding: "40px",
-            borderBottom: "1px solid #f0d0ff",
-          })}
-        >
-          <Link
-            href={"/"}
-            className={css({
-              textShadow: "0 10px 30px #aa00ff",
-              borderRadius: "5px",
-              padding: "10px",
-              color: "#f0d0ff",
-            })}
-          >
-            Nknight AMAMIYA
-          </Link>
-        </div>
-
-        <div
-          className={css({
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            padding: "10px",
-            borderBottom: "1px solid #f0d0ff",
+            background: "#050021",
+            color: "#f0d0ff",
           })}
         >
           <div
             className={css({
-              width: "100%",
               display: "flex",
               justifyContent: "center",
+              fontSize: "30px",
+              padding: "40px",
+              borderBottom: "1px solid #f0d0ff",
             })}
           >
             <Link
-              href={"/blog"}
+              href={"/"}
               className={css({
                 textShadow: "0 10px 30px #aa00ff",
                 borderRadius: "5px",
+                padding: "10px",
                 color: "#f0d0ff",
-                margin: "0 5px",
               })}
             >
-              Blog
-            </Link>
-
-            <Link
-              href={"/dev"}
-              className={css({
-                textShadow: "0 10px 30px #aa00ff",
-                borderRadius: "5px",
-                color: "#f0d0ff",
-                margin: "0 5px",
-              })}
-            >
-              Dev Projects
+              Nknight AMAMIYA
             </Link>
           </div>
-          <div
-            className={css({
-              width: "100%",
-              display: "flex",
-              justifyContent: "center",
-            })}
-          >
-            <Link
-              href={"/apps"}
-              className={css({
-                textShadow: "0 10px 30px #aa00ff",
-                borderRadius: "5px",
-                color: "#f0d0ff",
-                margin: "0 5px",
-              })}
-            >
-              Apps
-            </Link>
 
-            <Link
-              href={"/videos"}
-              className={css({
-                textShadow: "0 10px 30px #aa00ff",
-                borderRadius: "5px",
-                color: "#f0d0ff",
-                margin: "0 5px",
-              })}
-            >
-              Videos
-            </Link>
-          </div>
-        </div>
-        <main>{children}</main>
-        <footer>
           <div
             className={css({
               display: "flex",
-              justifyContent: "center",
               flexDirection: "column",
-              alignItems: "center",
-              padding: "7vh",
-              borderTop: "1px solid #f0d0ff",
-              background: "#000",
-              paddingBottom: "15vh",
+              justifyContent: "center",
+              padding: "10px",
+              borderBottom: "1px solid #f0d0ff",
             })}
           >
-            <div className={css({ marginBottom: "20px" })}>
-              <p>Share this page:</p>
-              <div
-                className={css({ display: "flex", justifyContent: "center", flexDirection: "column", gap: "10px" })}
+            <div
+              className={css({
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+              })}
+            >
+              <Link
+                href={"/blog"}
+                className={css({
+                  textShadow: "0 10px 30px #aa00ff",
+                  borderRadius: "5px",
+                  color: "#f0d0ff",
+                  margin: "0 5px",
+                })}
               >
-                <ShareButton />
-                <CopyButton />
-              </div>
+                Blog
+              </Link>
+
+              <Link
+                href={"/dev"}
+                className={css({
+                  textShadow: "0 10px 30px #aa00ff",
+                  borderRadius: "5px",
+                  color: "#f0d0ff",
+                  margin: "0 5px",
+                })}
+              >
+                Dev Projects
+              </Link>
             </div>
-            <div>(c) Nknight AMAMIYA 2021</div>
-            <Link
-              href={"https://varius.technology/"}
+            <div
               className={css({
-                borderRadius: "5px",
-                padding: "10px 40px",
-                color: "#f0d0ff",
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
               })}
             >
-              varius.technology
-            </Link>
+              <Link
+                href={"/apps"}
+                className={css({
+                  textShadow: "0 10px 30px #aa00ff",
+                  borderRadius: "5px",
+                  color: "#f0d0ff",
+                  margin: "0 5px",
+                })}
+              >
+                Apps
+              </Link>
 
-            <Link
-              href={"https://nknighta.me"}
-              className={css({
-                borderRadius: "5px",
-                padding: "10px 40px",
-                color: "#f0d0ff",
-              })}
-            >
-              nknighta.me
-            </Link>
-
-            <Link
-              href={"https://nknighta.me/vrchat"}
-              aria-label="Privacy Policy"
-              className={css({
-                borderRadius: "5px",
-                padding: "10px 40px",
-                color: "#f0d0ff",
-              })}
-            >
-              VRChat
-            </Link>
-            <Link
-              href={"/llmassets"}
-              className={css({
-                borderRadius: "5px",
-                padding: "10px 40px",
-                color: "#f0d0ff",
-              })}
-            >
-              LLM Assets
-            </Link>
-            <Link
-              href={"https://nknighta.me/privacy-policy"}
-              aria-label="Privacy Policy"
-              className={css({
-                borderRadius: "5px",
-                padding: "10px 40px",
-                color: "#f0d0ff",
-              })}
-            >
-              Privacy Policy
-            </Link>
+            </div>
+          </div>
+          <main>{children}</main>
+          <footer>
             <div
               className={css({
                 display: "flex",
                 justifyContent: "center",
                 flexDirection: "column",
                 alignItems: "center",
-                borderRadius: "5px",
-                padding: "10px 40px",
-                color: "#f0d0ff",
+                padding: "7vh",
+                borderTop: "1px solid #f0d0ff",
+                background: "#000",
+                paddingBottom: "15vh",
               })}
             >
-              - Mail -
+              <div className={css({ marginBottom: "20px" })}>
+                <p>Share this page:</p>
+                <div
+                  className={css({ display: "flex", justifyContent: "center", flexDirection: "column", gap: "10px" })}
+                >
+                  <ShareButton />
+                  <CopyButton />
+                </div>
+              </div>
+              <div>(c) Nknight AMAMIYA 2021</div>
               <Link
-                href={"mailto:nknighta@varius.technology"}
-                aria-label="Open email app"
+                href={"https://varius.technology/"}
                 className={css({
                   borderRadius: "5px",
                   padding: "10px 40px",
                   color: "#f0d0ff",
                 })}
               >
-                nknighta@varius.technology
+                varius.technology
               </Link>
+
+              <Link
+                href={"https://nknighta.me"}
+                className={css({
+                  borderRadius: "5px",
+                  padding: "10px 40px",
+                  color: "#f0d0ff",
+                })}
+              >
+                nknighta.me
+              </Link>
+
+              <Link
+                href={"https://nknighta.me/vrchat"}
+                aria-label="Privacy Policy"
+                className={css({
+                  borderRadius: "5px",
+                  padding: "10px 40px",
+                  color: "#f0d0ff",
+                })}
+              >
+                VRChat
+              </Link>
+              <Link
+                href={"/llmassets"}
+                className={css({
+                  borderRadius: "5px",
+                  padding: "10px 40px",
+                  color: "#f0d0ff",
+                })}
+              >
+                LLM Assets
+              </Link>
+              <Link
+                href={"https://nknighta.me/privacy-policy"}
+                aria-label="Privacy Policy"
+                className={css({
+                  borderRadius: "5px",
+                  padding: "10px 40px",
+                  color: "#f0d0ff",
+                })}
+              >
+                Privacy Policy
+              </Link>
+              <div
+                className={css({
+                  display: "flex",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  borderRadius: "5px",
+                  padding: "10px 40px",
+                  color: "#f0d0ff",
+                })}
+              >
+                - Mail -
+                <Link
+                  href={"mailto:nknighta@varius.technology"}
+                  aria-label="Open email app"
+                  className={css({
+                    borderRadius: "5px",
+                    padding: "10px 40px",
+                    color: "#f0d0ff",
+                  })}
+                >
+                  nknighta@varius.technology
+                </Link>
+              </div>
             </div>
-          </div>
-        </footer>
-      </div>
-    </ClickSpark>
+          </footer>
+        </div>
+      </ClickSpark>
+    </>
   );
 }
---
+++ src/pages/blog/page/[p].tsx
@@ -0,0 +1,159 @@
+// 必須: getStaticPaths を追加
+export const getStaticPaths = async () => {
+  // 総記事数取得
+  const data = await client.get({ endpoint: "blogs" });
+  const totalCount = data.totalCount || 0;
+  const PER_PAGE = 5;
+  const totalPages = Math.ceil(totalCount / PER_PAGE);
+
+  const paths = Array.from({ length: totalPages }, (_, i) => ({
+    params: { p: (i + 1).toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+import client from "../../../utils/cms";
+//BlogLayout
+import HMeta from "../../../components/headermeta";
+import Layout from "../../../layout/main";
+import { css } from "../../../../styled-system/css";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+export const getStaticProps = async ({context}) => {
+  const data = await client.get({ endpoint: "blogs", queries: { offset: context?.params?.page || 0, limit: 100 } });
+  console.log(Number(context?.params.p));
+  const categories = await client.get({ endpoint: "categories" });
+  //console.log(data.contents[2]);
+  return {
+    props: {
+      blog: data.contents,
+      categories: categories.contents,
+    },
+  };
+};
+
+const Category = ({ categories }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <div>
+      {!isMenuOpen ? (
+        <div>
+          <button onClick={() => setIsMenuOpen(true)}>Categories</button>
+        </div>
+      ) : (
+        <div>
+          <div className={
+            css(
+              {
+                position: "relative",
+                padding: "10px 20vw",
+                border: "2px solid #aa00ff",
+                borderRadius: "5px",
+                flexWrap: "wrap",
+                justifyContent: "center",
+                "& > div": {
+                  flex: "0 1 calc(33.333% - 20px)",
+                },
+                backgroundColor: "#000014",
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px",
+                marginTop: "10px",
+              }
+            )}>
+            <>
+              <button onClick={() => setIsMenuOpen(false)}>Categories</button>
+              {categories.map((category) => (
+                <div key={category.id}>
+                  <Link
+                    className={css({
+                      textShadow: "0 10px 30px #aa00ff",
+                      borderRadius: "5px",
+                      color: "#f0d0ff",
+                      padding: "5px 10px",
+                      backgroundColor: "#050021",
+                      transition: "background-color 0.3s ease-in-out",
+                      "&:hover": {
+                        backgroundColor: "#aa00ff",
+                        color: "#050021",
+                      },
+                    })}
+                    href={`/blog/category/${category.id}`}
+                  >
+                    {category.name}
+                  </Link>
+                </div>
+              ))}
+            </>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const Blog = ({ blog, categories }) => {
+  const [page, setPage] = useState(1);
+  return (
+    <Layout>
+      <HMeta
+        pageTitle="Blog"
+        pageDescription="Nknight AMAMIYA'S Blog"
+        pagePath="/blog"
+      />
+      <div
+        className={css({
+          p: 4,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "14px",
+        })}
+      >
+        <Category categories={categories} />
+        {blog.map((blog, category) => (
+          <div key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>
+              <Image
+                src={
+                  blog.eyecatch == null
+                    ? "https://images.microcms-assets.io/assets/a2939c8d25434ae5a1f853f2dc239a0f/b625a5435e8d4d18ab6c0b5499405b30/icon.jpeg?fit=fill&fill-color=000021&w=500&h=300"
+                    : blog.eyecatch.url +
+                    "?fit=fill&fill-color=000021&w=500&h=300"
+                }
+                alt="blog"
+                width={500}
+                height={300}
+                className={css({
+                  objectFit: "cover",
+                  objectPosition: "center",
+                  h: "300px",
+                  borderRadius: "10px",
+                  marginBottom: "20px",
+                  transition: "transform 0.3s ease-in-out",
+                  height: "auto",
+                })}
+              />
+              <h1>{blog.title}</h1>
+              {blog.category ? (
+                <p className={css({ color: "#f0d0ff" })}>
+                  Category: {blog.category.name}
+                </p>
+              ) : (
+                <p className={css({ color: "#f0d0ff" })}>No Category</p>
+              )}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
+};
+
+export default Blog;
