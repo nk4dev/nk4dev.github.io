6f694d9c191468866b4917a3607b5277b27ce4b4
nk4dev <nknighta@varius.technology>
2026-05-16 01:20:10 +0900

    feat: 🎸 custom 404, CLAUDE.md
    redirect app.nknighta.me, add CLAUDE.md

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,35 @@
-import HMeta from "../components/headermeta";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useSearchParams } from 'next/navigation'
 
 export default function Custom404() {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const [redirectUrl, setRedirectUrl] = useState("");
+    if (!searchParams) {
+        // Render fallback UI while search params are not yet available
+        return null;
+    }
+
+    const fallback = searchParams.get('notfoundfallback');
+    useEffect(() => {
+        const pathname = router.asPath;
+        if (!fallback) {
+            router.push("https://apps.nknighta.me/" + pathname);
+        }
+    }, [fallback, router]);
+
     return (
         <>
+        {redirectUrl}
             <div style={{ textAlign: "center", marginTop: "100px", marginBottom: "100px" }}>
                 <h1>Sorry!</h1>
             </div>
-
             <div style={{ textAlign: "center", marginTop: "100px", marginBottom: "100px" }}>
                 <h1>404 - Page Not Found</h1>
                 <p>The page you are looking for does not exist.</p>
-                <Link href="/" style={{textDecoration: "underline"}}>Go to Home</Link>
+                <Link href="/" style={{ textDecoration: "underline" }}>Go to Home</Link>
             </div>
             <div style={{ textAlign: "center", marginTop: "100px", marginBottom: "100px" }}>
                 &copy; 2021 - {new Date().getFullYear()} Nknight AMAMIYA@nk4dev
---
+++ src/pages/nk4dev/index.tsx
@@ -0,0 +1,10 @@
+import Layout from "../../layout/main";
+import LinkPage from "../../components/redirect";
+
+export default function Nk4dev() {
+  return (
+    <Layout>
+      <LinkPage url="/" text="Redirecting..." />
+    </Layout>
+  );
+}
